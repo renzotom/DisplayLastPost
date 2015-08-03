3a62390fe9354293d8dbe61f7c904c204d7bbff0
Aurelienazerty <Aurelienazerty@users.noreply.github.com>
2015-08-03 11:08:22 +0200

    1.1.0

--- event/listener.php
+++ event/listener.php
@@ -15,7 +15,8 @@ namespace Aurelienazerty\DisplayLastPost\event;
  */
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class listener implements EventSubscriberInterface {
+class listener implements EventSubscriberInterface
+{
 	
 	/** @var \phpbb\db\driver\driver_interface */
 	protected $db;
@@ -38,40 +39,46 @@ class listener implements EventSubscriberInterface {
 	 * @return \Aurelienazerty\DisplayLastPost\event\listener
 	 * @access public
 	 */
-	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\config\config $config, \phpbb\user $user) {
+	public function __construct(\phpbb\db\driver\driver_interface $db, \phpbb\config\config $config, \phpbb\user $user)
+	{
 		$this->user = $user;
 		$this->config = $config;
 		$this->db = $db;
 	}
 
-	static public function getSubscribedEvents() {
+	static public function getSubscribedEvents()
+	{
 		return array(
-			'core.viewtopic_get_post_data'				=> 'modify_viewtopic_post_list',
-			//Modified priority for FirstPostOnEveryPage ext compatibility.
-			'core.viewtopic_modify_post_row'			=> array('modify_first_post_of_the_topic', -2710),
-			'core.acp_board_config_edit_add'			=> 'acp_board_post_config',
+			'core.viewtopic_get_post_data'		=> 'modify_viewtopic_post_list',
+			'core.viewtopic_modify_post_row'	=> array('modify_first_post_of_the_topic', -2710),
+			'core.acp_board_config_edit_add'	=> 'acp_board_post_config',
 		);
 	}
 	
-	public function modify_first_post_of_the_topic($event) {
+	public function modify_first_post_of_the_topic($event)
+	{
 		$start = $event['start'];
-		if ($this->config['display_last_post_show'] && $start > 0 && $event["post_row"]["POST_ID"] == $this->last_post_id) {
+		if ($this->config['display_last_post_show'] && $start > 0 && $event["post_row"]["POST_ID"] == $this->last_post_id)
+		{
 			$this->user->add_lang_ext('Aurelienazerty/DisplayLastPost', 'display_last_post');
 			$post_row = $event['post_row'];
-			$post_row['MESSAGE'] = '<p style="font-weight: bold; margin-bottom: 1em;font-size: 1em;">' . $this->user->lang['DISPLAY_LAST_POST_TEXT'] . $this->user->lang['COLON'] . '</p>' . $post_row['MESSAGE'];
+			$post_row['MESSAGE'] = '<p style="font-weight: bold; font-size: 1em;">' . $this->user->lang['DISPLAY_LAST_POST_TEXT'] . $this->user->lang['COLON'] . '</p>' . $post_row['MESSAGE'];
 			$event['post_row'] = $post_row;
 		}
 	}
 
-	public function modify_viewtopic_post_list($event) {
+	public function modify_viewtopic_post_list($event)
+	{
 		$topic_data = $event['topic_data'];
 		$start = $event['start'];
 		$sql_ary = $event['sql_ary'];
 		$post_list = $event['post_list'];
-		if ($this->config['display_last_post_show'] && $start > 0) {
+		if ($this->config['display_last_post_show'] && $start > 0)
+		{
 			$posts_per_page = $this->config['posts_per_page'];
 			$new_post_list = array();
-			foreach ($post_list as $key => $value) {
+			foreach ($post_list as $key => $value)
+			{
 				$new_post_list[$key+1] = $value;
 			}
 			$sql_array = array(
@@ -94,15 +101,17 @@ class listener implements EventSubscriberInterface {
 	}
 
 	// ACP functions
-	public function acp_board_post_config($event) {
-		if ($event['mode'] == 'post') {
+	public function acp_board_post_config($event)
+	{
+		if ($event['mode'] == 'post')
+		{
 			$display_vars = $event['display_vars'];
 			$add_config_var = array(
 				'display_last_post_show'	=> array(
 					'lang' => 'DISPLAY_LAST_POST_SHOW',
 					'validate' => 'bool',
 					'type' => 'radio: yes_no',
-					'explain' => true
+					'explain' => true,
 				)
 			);
 			$display_vars['vars'] = phpbb_insert_config_array($display_vars['vars'], $add_config_var, array('after' =>'posts_per_page'));
